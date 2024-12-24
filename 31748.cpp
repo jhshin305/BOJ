8c44a43a1eb333679a42c067e96c6a76cacf8334
JongHoon <jhshin305@gmail.com>
2024-12-24 16:48:28 +0900

    final

--- 31748.cpp
+++ 31748.cpp
@@ -12,14 +12,22 @@ int main() {
 	
 	freopen("C:/Users/jhshin/Desktop/secret/BOJ/input.txt", "r", stdin);
 	
+	while(true) {/////////////////////////
 	int n;
 	cin >> n;
-	vector<int> a(n);
+	if(n==0) break;//////////////////////////
+	vector<ll> a(n);
 	for (int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
-
 	
-	
+	vector<ll> ans(n+1);
+	ans[0] = 1;
+	for (int i=0; i<n; i++) {
+		if(i+a[i] < n) ans[i+a[i]+1] = (ans[i+a[i]+1] + ans[i])mod;
+		if((i-a[i] >= 0) && (a[i] != a[i-a[i]])) ans[i+1] = (ans[i+1] + ans[i-a[i]])mod;
+	}
+	cout << ans.back() << endl;
+	}////////////////////////
 	return 0;
 }
