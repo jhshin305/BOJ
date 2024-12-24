fd05a0691f63c452c4d3a9d20b32fdd61b0665d4
JongHoon <jhshin305@gmail.com>
2024-12-24 14:02:31 +0900

    init

---
+++ 31748.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define mod % 1000000007
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	freopen("C:/Users/jhshin/Desktop/secret/BOJ/input.txt", "r", stdin);
+	
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	
+	
+	return 0;
+}
