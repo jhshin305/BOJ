ab9ba98a0af08017df3177567a081747e1f19355
JongHoon <jhshin305@gmail.com>
2025-01-06 19:28:00 +0900

    final

---
+++ 24265.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define mod % 1000000007
+
+#ifndef BOJ
+#include <cstdio>
+#endif
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	#ifndef BOJ
+	freopen("C:/Users/jhshin/Desktop/secret/BOJ/input.txt", "r", stdin);
+	while(true) {
+	#endif
+	
+	ll n;
+	cin >> n;
+	cout << n*(n-1)/2 << endl << 2 << endl;
+	
+	
+	#ifndef BOJ
+	}
+	#endif
+	
+	return 0;
+}
